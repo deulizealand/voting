99f0865b3bb5729e1cdb4341878674959bef1189
VM <4952512+vm-solusi@users.noreply.github.com>
2021-04-29 09:05:54 +0700

    Update Hitungan Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -157,6 +157,22 @@ class DashboardController extends Controller
             $calons = Participant::select(\DB::raw('participants.id,participants.position_id,
                     ifnull((select count(id) as jml from votings where votings.participant_id=participants.id and participants.position_id=votings.position_id group by id),0) as total'))
                     ->join('positions','positions.id','=','participants.position_id')
+                    ->where('participants.position_id',1)
+                    ->get();
+
+            return response()->json($calons);
+        }
+        
+    }
+    
+    public function refreshPemilihPengawas(Request $request)
+    {
+        if($request->ajax())
+        {
+            $calons = Participant::select(\DB::raw('participants.id,participants.position_id,
+                    ifnull((select count(id) as jml from votings where votings.participant_id=participants.id and participants.position_id=votings.position_id group by id),0) as total'))
+                    ->join('positions','positions.id','=','participants.position_id')
+                    ->where('participants.position_id',2)
                     ->get();
 
             return response()->json($calons);
